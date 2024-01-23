f3ac112693cc8ff5f23fef2f18bc6df81b877942
Marisol Castillo <36350945@fi.unju.edu.ar>
2024-01-23 04:59:58 -0300

    Se borraron archivos repetidos y se mejoró la estructura

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('node:path');
 
-
+app.use(express.static('public'))
 
 app.listen( 3002, () => console.log(`Server up on port: http://localhost:3002`) )
 
