e6faf5273a86dd1352974d407d3e345117161877
Marisol Castillo <36350945@fi.unju.edu.ar>
2024-01-23 05:20:28 -0300

    Se corrigieron errores en app.js

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ app.use(express.static('public'))
 app.listen( 3002, () => console.log(`Server up on port: http://localhost:3002`) )
 
 app.get('/', (req, res) =>{
-    const pathHome = path.join(__dirname, 'app/views/home.html')
+    const pathHome = path.join(__dirname, 'views/home.html')
     res.sendFile(pathHome);
 } )
 
 app.get('/registro', (req, res) =>{
-    const pathHome = path.join(__dirname, 'app/views/register.html')
+    const pathHome = path.join(__dirname, 'views/register.html')
     res.sendFile(pathHome);
 } )
 
 app.get('/carrito', (req, res) =>{
-    const pathHome = path.join(__dirname, 'app/views/carrito.html')
+    const pathHome = path.join(__dirname, 'views/carrito.html')
     res.sendFile(pathHome);
 } )
 
 app.get('/catalogo', (req, res) =>{
-    const pathHome = path.join(__dirname, 'app/views/catalogo.html')
+    const pathHome = path.join(__dirname, 'views/catalogo.html')
     res.sendFile(pathHome);
 } )
 
 app.get('/contacto', (req, res) =>{
-    const pathHome = path.join(__dirname, 'app/views/contact.html')
+    const pathHome = path.join(__dirname, 'views/contact.html')
     res.sendFile(pathHome);
 } )
 
 app.get('/sobre-nosotros', (req, res) =>{
-    const pathHome = path.join(__dirname, 'app/views/about.html')
+    const pathHome = path.join(__dirname, 'views/about.html')
     res.sendFile(pathHome);
 } )
 
 app.get('/producto', (req, res) =>{
-    const pathHome = path.join(__dirname, 'app/views/producto.html')
+    const pathHome = path.join(__dirname, 'views/producto.html')
     res.sendFile(pathHome);
 } )
 
