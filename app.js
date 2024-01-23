bfd353ea06ab28d6289ee8cd835d01f6a869bc42
Marisol Castillo <36350945@fi.unju.edu.ar>
2024-01-23 04:00:04 -0300

    Se agregaron las vistas, los archivos css e imagenes necesarias en la página

--- app.js
+++ app.js
@@ -10,3 +10,35 @@ app.get('/', (req, res) =>{
     const pathHome = path.join(__dirname, 'app/views/home.html')
     res.sendFile(pathHome);
 } )
+
+app.get('/registro', (req, res) =>{
+    const pathHome = path.join(__dirname, 'app/views/register.html')
+    res.sendFile(pathHome);
+} )
+
+app.get('/carrito', (req, res) =>{
+    const pathHome = path.join(__dirname, 'app/views/carrito.html')
+    res.sendFile(pathHome);
+} )
+
+app.get('/catalogo', (req, res) =>{
+    const pathHome = path.join(__dirname, 'app/views/catalogo.html')
+    res.sendFile(pathHome);
+} )
+
+app.get('/contacto', (req, res) =>{
+    const pathHome = path.join(__dirname, 'app/views/contact.html')
+    res.sendFile(pathHome);
+} )
+
+app.get('/sobre-nosotros', (req, res) =>{
+    const pathHome = path.join(__dirname, 'app/views/about.html')
+    res.sendFile(pathHome);
+} )
+
+app.get('/producto', (req, res) =>{
+    const pathHome = path.join(__dirname, 'app/views/producto.html')
+    res.sendFile(pathHome);
+} )
+
+
