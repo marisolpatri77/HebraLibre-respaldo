61494aeb1e2c2ded545e79350d2fd80a9c5ed375
Andres <andres.silva.09@hotmail.com>
2024-02-15 13:39:15 -0300

    1-Se agrega carpeta Models con archivo productos.js.2-Se agrega carga dinamica de catalogo y producto.3-Se agrega metodos detalle y catalogo en controlador de producto.js4-Se deja comentado linea 6 de routes/producto.js para corregir salida de error

--- src/controllers/productos.js
+++ src/controllers/productos.js
@@ -1,13 +1,26 @@
+
+const data = require('../Models/productos.json');
+
+
 const controllerProductos = {
-    producto: (req, res) =>{
-        res.render('producto');
-    },
+   
     carrito: (req, res) =>{
-        res.render('carrito');
+        res.render('carrito',{data});
     },
     catalogo: (req, res) =>{
-        res.render('catalogo');
+        res.render('catalogo',{data:data});
+    },
+    
+   detalle: (req, res) =>{
+
+    let idProducto= req.params.id;
+    let idBuscado= data.find(indice=>indice.id === idProducto);
+   
+        res.render('producto',{idBuscado});
     }
+
 }
 
 module.exports = controllerProductos
+
+
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -3,11 +3,13 @@ let router = express.Router();
 
 let controllerProductos = require("../controllers/productos")
 
-router.get('/producto', controllerProductos.producto)
+//router.get('/producto', controllerProductos.producto)
 
 router.get('/carrito', controllerProductos.carrito)
 router.get('/producto/carrito', controllerProductos.carrito)
 
 router.get('/catalogo', controllerProductos.catalogo)
+
+router.get('/detalle/:id', controllerProductos.detalle)
 
 module.exports = router
